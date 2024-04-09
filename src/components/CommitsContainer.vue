875c68d4c0f64d429bca6e38f1543755221b1929
lizzli <restemstelios@yahoo.com>
2024-04-09 21:40:58 +0300

    wip

--- src/components/CommitsContainer.vue
+++ src/components/CommitsContainer.vue
@@ -3,67 +3,91 @@ const GITHUB_ENDPOINT = "https://api.github.com/repos/";
 const API_PATH = "/core/commits?per_page=30&sha=main";
 
 export default {
+  
   data: () => ({
     repo: "vuejs",
-    commits: [],
+    commits: null,
+    error: null,
+    loading: false,
   }),
   mounted() {
     this.fetchRepoInfo();
   },
   watch: {
     repo: "fetchRepoInfo",
+    
   },
   methods: {
     fetchRepoInfo() {
+      this.commits = null;
+      this.loading = true;
+      this.error = null;
       fetch(GITHUB_ENDPOINT + this.repo + API_PATH)
         .then((res) => {
-          return res.json();
+          if(res.status !== 200) {
+              throw ("An error occured!")
+          }
         })
         .then((result) => {
           this.commits = result;
+          this.loading = false;
         })
         .catch((err) => {
           console.error(err);
+          this.error = err;
+          this.loading = false;
         });
     },
+    computed: {
+  console: () => console,
+    },
     truncate(v) {
+      if (!v) return;
       const newline = v.indexOf("\n");
-      return newline > 0 ? v.slice(0, newline) : v;
+      return newline > 0 ? v?.slice(0, newline) : v;
     },
     formatDate(v) {
       return v.replace(/T|Z/g, " ");
+    },
+    updateRepoItem(e) {
+      this.repo = e.target.value;
     },
   },
 };
 </script>
 
 <template>
+  <input :value="repo" @input="updateRepoItem" />
   <div class="commits">
     <div class="commits-container">
-      <ul>
-        <li
-          v-for="{ html_url, sha, author, commit } in commits"
-          class="commit-container"
-        >
-          <a :href="html_url" target="_blank" class="commit">
-            {{ sha.slice(0, 7) }}</a
+      <div v-if="loading && !error">Loading...</div>
+      <div v-else-if="!error && commits">
+        <ul>
+          <li
+            v-for="{ html_url, sha, author, commit } in commits"
+            class="commit-container"
           >
-          <br>
-          <span class="commit-message">
-            {{ truncate(commit.message) }}
-          </span>
-          <br />
-          <span class="author">
-            <a :href="author.html_url" target="_blank">
-              {{ commit.author.name }}
-            </a>
-          </span>
-          <br>
-          <span class="date">
-            {{ commit.author.date }}
-          </span>
-        </li>
-      </ul>
+            <a :href="html_url" target="_blank" class="commit">
+              {{ sha.slice(0, 7) }}</a
+            >
+            <br />
+            <span class="commit-message">
+              {{ truncate(commit.message) }}
+            </span>
+            <br />
+            <span class="author">
+              <a :href="author.html_url" target="_blank">
+                {{ commit.author.name }}
+              </a>
+            </span>
+            <br />
+            <span class="date">
+              {{ commit.author.date }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div v-else>{{ error }}</div>
     </div>
   </div>
 </template>
